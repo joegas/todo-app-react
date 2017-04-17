4308982248a8d651cb119396b83babc947fc7cac
mario <mariowalz@me.com>
2017-04-17 13:46:48 +0200

    added Form component

---
+++ src/components/Form.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {text: ""}
+}
+    render() {
+        return (
+            <div>
+              <form onSubmit={this.handleSubmit}>
+                <input onChange={this.handleChange} value={this.state.text} />
+                <button>Add</button>
+              </form>
+            </div>
+        );
+    }
+
+    handleChange(e) {
+      this.setState({text: e.target.value});
+    }
+
+    handleSubmit(e) {
+      e.preventDefault();
+      var newItem = {
+        text: this.state.text,
+        id: Date.now()
+      };
+      this.props.callbackParent(newItem)
+    }
+}
+
+export default Form;
