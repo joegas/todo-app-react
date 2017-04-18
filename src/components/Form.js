bfb87fafc24c2e413a9d13334855b13f6d63a564
ChristianGutwein <ChristianGutwein@gmx.de>
2017-04-18 17:22:15 +0200

    update comments

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,17 @@
 import React, { Component } from 'react';
 
+// ÜBUNG 3
+//
+// 1. Definiere im 'constructor' den State und weise den State dem Input über das Value-Attribut zu.
+//
+// 2. Sobald sich das Input-Field ändert, soll der Wert im State gespeichert werden. Füge hierzu den Event-Handler 'onChange'
+//    im bereits angelegten 'Input-Field' hinzu, der die Funktion handleChange aufruft (mittels 'this.handleChange').
+//    In dieser Funktion soll der State 'text' neu gesetzt werden.
+//
+// 3. Beim Klick auf den Button wollen wir den eingegeben Wert an die App-Komponente übermitteln. benutze dafür den Event-Handler
+//    'onSubmit', der 'handleChange' aufruft.
+
+
 class Form extends Component {
   constructor(props) {
     super(props);
