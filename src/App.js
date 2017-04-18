764100102f825de07600d4c86b46fac8e69dc0fb
mario <mariowalz@me.com>
2017-04-18 16:52:05 +0200

    set base for exercise 1

--- src/App.js
+++ src/App.js
@@ -3,46 +3,34 @@ import List from './components/List.js';
 import Form from './components/Form.js';
 import uuid from 'uuid';
 
-
 class App extends Component {
   constructor(){
     super();
     this.onChildChanged = this.onChildChanged.bind(this);
-    this.state = {
-      tickets: []
-    }
   }
 
   onChildChanged(newItem) {
     this.setState((prevState) => ({
-      tickets: prevState.tickets.concat(
+      todos: prevState.todos.concat(
         {
           id: uuid.v4(),
-          text: newItem.text
+          text: newItem
         })
     }));
   }
 
   componentDidMount() {
-    this.setState({
-      tickets: [
-        {id: "1", text: "To Do 1"},
-        {id: "2", text: "To Do 2"},
-      ]
-    });
+
   }
 
   componentWillUnmount() {
-    this.setState({
-      tickets: []
-    });
+
   }
 
   render() {
     return (
       <div>
-        <List listItems={this.state.tickets} />
-        <Form callbackParent={this.onChildChanged} />
+        <List />
       </div>
     );
   }
--- src/components/List.js
+++ src/components/List.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 
+// 1. Lege im Konstruktor ein Array als State an. Es soll Objekte mit jeweils zwei Feldern (ID, Titel) aufnehmen.
+// 2. Zeige die Objekte anhand des Namens in einer Liste (map-Funktion!). Vergesse dabei nicht den eindeutigen Key anzugeben.
+
 class List extends Component {
+    constructor() {
+      super();
+    }
+
     render() {
         return (
             <ul>
-            {this.props.listItems.map(item => (
-                <li key={item.id}>{item.text}</li>
-            ))}
+              // ...
             </ul>
         );
     }
