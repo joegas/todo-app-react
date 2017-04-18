7897357e44b892e4020d00a1ebabdcd9aa3ca02f
Vali <Vali_Schagerl@web.de>
2017-04-18 17:20:34 +0200

    Added ex2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import List from './components/List.js';
 import Form from './components/Form.js';
 import uuid from 'uuid';
 
+// ÜBUNG 2
+//
+// 1. Erstelle im constructor einen State mit einem Feld (z.B. todos), das ein vorerst leeres Array beinhaltet
+// 2. Dieses Array soll nun in der Methode componentDidMount() gefüllt werden. Dafür kannst das Array verwenden, 
+//    das du bereits in der List Komponente erstellt hast.
+// 3. Übergebe dieses Array als prop an die List Komponente. 
+//    Die List Komponente muss zudem angepasst werden (props empfangen und den state dort entfernen).
+
 
 class App extends Component {
   constructor(){
