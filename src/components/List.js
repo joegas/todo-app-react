23d5a13d4215ffa74fbebb4118708b79e9d5233c
mario <mariowalz@me.com>
2017-04-18 19:03:19 +0200

    update

--- src/components/List.js
+++ src/components/List.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 
 class List extends Component {
-    render() {
-        return (
-            <ul>
-            {this.props.listItems.map(item => (
-                <li key={item.id}>{item.text}</li>
-            ))}
-            </ul>
-        );
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {id: "1", text: "To Do 1"},
+        {id: "2", text: "To Do 2"}
+      ]
     }
+  }
+
+  render() {
+    return (
+        <ul>
+        {this.state.todos.map(item => (
+            <li key={item.id}>{item.text}</li>
+        ))}
+        </ul>
+    );
+  }
 }
 
 export default List;
